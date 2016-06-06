be29f56de072e31d5872de8262f6d7509a45ad7b
Venkat Nagappan <wenkatn@gmail.com>
2016-06-06 12:38:53 -0400

    Added htmls for welcome and initial install. No logic yet

---
+++ config.py
@@ -0,0 +1,17 @@
+#################################
+## MyBanker Configuration File ##
+#################################
+
+# Whether initial installation wizard ran and configured the application
+# done - Initial setup is completed and the app is ready to use
+# pending - Initial setup not completed and application will lauch setup wizard 
+#           next time the site is accessed
+INITIAL_SETUP = 'pending'
+
+# ---------------- #
+# DATABASE DETAILS
+# ---------------- #
+DB_HOST = ''
+DB_USER = ''
+DB_PASS = ''
+DB_NAME = 'mybanker'
--- mybanker.py
+++ mybanker.py
@@ -4,11 +4,22 @@ from flask import Flask, render_template
 # Initialize Flask object
 app = Flask(__name__)
 
+# Load configuration from file
+app.config.from_object('config')
+
 # Index Route
 @app.route('/')
 def index():
-  return render_template('index.html')
+  if app.config['INITIAL_SETUP'].lower() != 'done':
+    return render_template('install_welcome.html')
+  else:
+    return render_template('index.html')
+
+# Initial Install Route
+@app.route('/setup', methods=['GET', 'POST'])
+def setup():
+  return render_template('install_welcome_details.html')
 
 # Main Function
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port='8002')
+  app.run(port=8002, debug=True)
